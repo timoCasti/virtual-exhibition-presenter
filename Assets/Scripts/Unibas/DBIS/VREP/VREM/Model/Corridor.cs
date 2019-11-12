e649dadc46ec446576d3bb2f1c66c8d3c1aa130e
GowthamanG <gowthaman.gobalasingam@gmail.com>
2019-11-12 18:37:50 +0100

    Removed inheritance from Room, fully implemented now

--- Assets/Scripts/Unibas/DBIS/VREP/VREM/Model/Corridor.cs
+++ Assets/Scripts/Unibas/DBIS/VREP/VREM/Model/Corridor.cs
@@ -5,8 +5,49 @@ using World;
 
 namespace DefaultNamespace.VREM.Model
 {
-    public class Corridor : Room
+    [Serializable]
+    public class Corridor
     {
+        public string text;
+        public Vector3 size;
+        public Vector3 position;
+        public Vector3 entrypoint;
+        public Wall[] walls;
+
+        public string floor;
+        public string ceiling;
+
+        public string ambient;
+
+        public Exhibit[] exhibits;    
+    
+    
+        public string GetURLEncodedAudioPath() {
+            if (!string.IsNullOrEmpty(ambient))
+            {
+                return ServerSettings.SERVER_ID+"content/get/"+ ambient.Substring(0).Replace("/", "%2F").Replace(" ", "%20");
+            }
+            else
+            {
+                return null;
+            }
+      
+        }
+
+        public Wall GetWall(WallOrientation orientation)
+        {
+            foreach (var wall in walls)
+            {
+                WallOrientation wor = (WallOrientation) Enum.Parse(typeof(WallOrientation), wall.direction, true);
+                if (wor.Equals(orientation))
+                {
+                    return wall;
+                }
+            }
+
+            return null;
+        }
+    
         
     }
 }
